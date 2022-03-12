ff9f5ec527c2017b8fa7f566584881daca0844b9
Artem Prokop <artem13.prokop@gmail.com>
2022-03-12 14:05:20 +0200

    🔧 Optimize winners detection

--- src/services/end-game.js
+++ src/services/end-game.js
@@ -1,8 +1,8 @@
 async function endGame(game, Player) {
 	const playerIds = game.players.map(player => player.userId)
 	const players = await Player.getMany(playerIds)
-	const mafiaAlive = players.some(player => player.isMafia)
-	const winners = mafiaAlive ? 'mafia' : 'peaceful'
+	const peacefulAlive = players.some(player => !player.isMafia)
+	const winners = peacefulAlive ? 'peaceful' : 'mafia'
 	await Player.deleteMany({
 		userId: {
 			$in: playerIds
